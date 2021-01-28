466d0fea88c624e9c3a70ae456704b477c19e4bf
Bryan De La Cruz <bdelacruza@pucp.edu.pe>
2021-01-28 00:19:14 -0500

    creando el primer componente

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Parrafo from './components/Parrafo.jsx'
+
+function App() {
+  return (
+    <div>
+      <h1>Hola mundo React</h1>
+      <Parrafo/>
+    </div>
+  );
+}
+
+export default App;
